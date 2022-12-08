02014dade0018496c22196796cafe6e0c883d34e
Jon Wedell <jon@jonwedell.com>
2022-12-08 12:15:19 -0600

    Working code which prints the full energy usage as CSV.

--- vced_stats.py
+++ vced_stats.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
+import csv
 import json
 import math
 import os
 import pathlib
-import sys
 import time
 from contextlib import closing
-from typing import List
+from io import StringIO
+from typing import List, Tuple
 
 import grpc
 
@@ -51,69 +52,102 @@ def write_to_db():
             result = cur.fetchall()
 
 
-def usage_request(vue2: DeviceInventoryResponse.Device.DeviceModel.Vue2, now: int) -> List[dict]:
-    """ Creates and executes a usage request getting details for a particular
-    device. Returns usage for all circuits as a list of dictionaries, with
-     the circuit info combined with usage. (Why didn't they design the API this
-     way?) """
+def get_detailed_usage() -> (List[List[dict]], int):
+    """ Gets usage info for all circuits on all devices. Returns usage for all circuits as a
+    list of a list of dictionaries, with the circuit info combined with usage.
+    (Why didn't they design the API so that you don't have to combine the circuit types
+    manually?) """
 
+    now = math.ceil(time.time())
     usage_request = DeviceUsageRequest()
     usage_request.auth_token = auth_token
     usage_request.start_epoch_seconds = now - 900  # one fifteen minute period
     usage_request.end_epoch_seconds = now
     usage_request.scale = DataResolution.FifteenMinutes
     usage_request.channels = DeviceUsageRequest.UsageChannel.ALL
-    usage_request.manufacturer_device_ids.append(vue2.manufacturer_device_id)
+    usage_request.manufacturer_device_ids.extend([_.manufacturer_device_id for _ in devices])
 
     usage_response = stub.GetUsageData(usage_request)
 
+    timestamp = None
     response = []
-    for circuit in vue2.circuit_infos:
-        circuit_data = {'channel_number': circuit.channel_number,
-                        'direction': circuit.energy_direction}
-        if circuit.channel_number < 4:
-            circuit_data['type'] = 'Mains'
-        else:
-            circuit_data['type'] = circuit.sub_type
-        for channel in usage_response.device_usages[0].channel_usages:
-            if channel.channel == circuit.channel_number:
-                circuit_data['usage'] = channel.usages[0]
-        # Only return circuits with usage
-        if 'usage' in circuit_data:
-            response.append(circuit_data)
+    for usage_data in usage_response.device_usages:
 
-    return response
+        # Determine what period this data is for
+        if usage_data.bucket_epoch_seconds[0] != timestamp:
+            if timestamp is not None:
+                print('Mismatched data...', timestamp, usage_data.bucket_epoch_seconds[0])
+            timestamp = usage_data.bucket_epoch_seconds[0]
+
+        circuits = []
+        for vue2 in devices:
+            if usage_data.manufacturer_device_id != vue2.manufacturer_device_id:
+                continue
+            for circuit in vue2.circuit_infos:
+                circuit_data = {'channel_number': circuit.channel_number,
+                                'direction': circuit.energy_direction}
+                if circuit.channel_number < 4:
+                    circuit_data['type'] = 'Mains'
+                else:
+                    circuit_data['type'] = circuit.sub_type
+                for channel in usage_response.device_usages[0].channel_usages:
+                    if channel.channel == circuit.channel_number:
+                        circuit_data['usage'] = channel.usages[0]
+                # Only return circuits with usage
+                if 'usage' in circuit_data:
+                    circuits.append(circuit_data)
+        response.append(circuits)
+
+    return response, timestamp
 
 
-# (Currently known) Circuit types
-def get_usage_by_circuit_type(usage_data: List[dict], circuit_type: str):
+def get_usage_by_circuit_type(usage_data: List[List[dict]], circuit_type: str) -> Tuple[float, int]:
     """
     Currently known circuit types:
-    'Clothes Dryer', 'Cooktop/Range/Oven/Stove', 'Clothes Washer', 'Battery',
+    'Mains', 'Clothes Dryer', 'Cooktop/Range/Oven/Stove', 'Clothes Washer', 'Battery',
     'Water Heater', 'Humidifier/Dehumidifier', 'Fridge/Freezer','Air Conditioner',
     'Boiler', 'Lights', 'Sub Panel', 'Heat Pump', 'Dishwasher', 'Other', 'Microwave',
     'Pump', 'Garage/Shop/Barn/Shed', 'Kitchen', 'Solar/Generation', 'Room/Multi-use Circuit',
     'Electric Vehicle/RV', 'Hot Tub/Spa', 'Computer/Network', 'Furnace'
+
+    Returns the total usage of circuits of the type, and the number of devices
+    with circuits of the type. Note: multiple circuits in one home may be of the same type.
     """
     total_usage = 0
-    found_circuits = 0
+    found_homes_with_circuit = 0
 
     for device in usage_data:
+        device_found_circuit = False
         for circuit in device:
             if circuit['type'] == circuit_type:
                 total_usage += circuit['usage']
-                found_circuits += 1
-    return total_usage, found_circuits
+                device_found_circuit = True
+        if device_found_circuit:
+            found_homes_with_circuit += 1
+    return total_usage, found_homes_with_circuit
 
 
-def get_detailed_energy_details():
+def get_detailed_energy_csv() -> str:
+    usage, timestamp = get_detailed_usage()
 
-    now = math.ceil(time.time())
+    # Determine the unique circuit types
+    types = set()
+    for device in usage:
+        for circuit in device:
+            types.add(circuit['type'])
 
-    print(usage_request(devices[0], now))
+    # Determine usage for each circuit type
+    summary = []
+    for circuit_type in types:
+        type_data = get_usage_by_circuit_type(usage, circuit_type)
+        summary.append({'usage': type_data[0], 'homes': type_data[1], 'circuit_type': circuit_type, 'timestamp': timestamp})
 
-    #all_usages = [usage_request(device, now) for device in devices[:5]]
-    pass
+    # Render results as CSV
+    csv_file = StringIO()
+    csv_writer = csv.DictWriter(csv_file, fieldnames=['timestamp', 'circuit_type', 'usage', 'homes'])
+    csv_writer.writerows(summary)
+    csv_file.seek(0)
+    return csv_file.read()
 
 
 def get_energy_summary():
@@ -146,6 +180,5 @@ def get_energy_summary():
 
 if __name__ == "__main__":
 
-    #get_detailed_energy_details()
-    get_energy_summary()
-    sys.exit(0)
+    get_detailed_energy_csv()
+    #get_energy_summary()

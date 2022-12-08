57cb69197c96655d12cef9eb77dff5bf204e40ff
Jon Wedell <jon@jonwedell.com>
2022-12-08 11:35:44 -0600

    More improvements and cleanup. Summary works, but individual device response makes too many API calls.

--- vced_stats.py
+++ vced_stats.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 import json
 import math
 import os
@@ -7,6 +6,7 @@ import pathlib
 import sys
 import time
 from contextlib import closing
+from typing import List
 
 import grpc
 
@@ -39,14 +39,8 @@ inventoryRequest = DeviceInventoryRequest()
 inventoryRequest.auth_token = auth_token
 inventoryResponse = stub.GetDevices(inventoryRequest)
 
-# Devices include both outlets and vue meters...
-devices = inventoryResponse.devices
-
-# Get the list of vue2s
-vue2_list = [dev for dev in devices if dev.model == DeviceInventoryResponse.Device.DeviceModel.Vue2 and dev.device_connected]
-
-# Prepare the results
-results = {}
+# Get the list of active vue2 devices
+devices = [dev for dev in inventoryResponse.devices if dev.model == DeviceInventoryResponse.Device.DeviceModel.Vue2]
 
 
 def write_to_db():
@@ -57,22 +51,69 @@ def write_to_db():
             result = cur.fetchall()
 
 
-def get_heater_energy(vue2: DeviceInventoryResponse.Device.DeviceModel.Vue2):
-    deviceUsageRequest = DeviceUsageRequest()
-    deviceUsageRequest.auth_token = auth_token
+def usage_request(vue2: DeviceInventoryResponse.Device.DeviceModel.Vue2, now: int) -> List[dict]:
+    """ Creates and executes a usage request getting details for a particular
+    device. Returns usage for all circuits as a list of dictionaries, with
+     the circuit info combined with usage. (Why didn't they design the API this
+     way?) """
 
-    now = math.ceil(time.time())  # seconds as integer
-    deviceUsageRequest.start_epoch_seconds = now - 900  # one fifteen minute period
-    deviceUsageRequest.end_epoch_seconds = now
-    deviceUsageRequest.scale = DataResolution.FifteenMinutes
-    deviceUsageRequest.channels = DeviceUsageRequest.UsageChannel.MAINS
-    deviceUsageRequest.manufacturer_device_ids.append(vue2.manufacturer_device_id)
+    usage_request = DeviceUsageRequest()
+    usage_request.auth_token = auth_token
+    usage_request.start_epoch_seconds = now - 900  # one fifteen minute period
+    usage_request.end_epoch_seconds = now
+    usage_request.scale = DataResolution.FifteenMinutes
+    usage_request.channels = DeviceUsageRequest.UsageChannel.ALL
+    usage_request.manufacturer_device_ids.append(vue2.manufacturer_device_id)
 
-    usage_response = stub.GetUsageData(deviceUsageRequest)
+    usage_response = stub.GetUsageData(usage_request)
 
-    for device_usage in usage_response.device_usages:
-        mains_usage = sum([_.usages[0] for _ in device_usage.channel_usages])
-        print(device_usage.bucket_epoch_seconds[0], mains_usage)
+    response = []
+    for circuit in vue2.circuit_infos:
+        circuit_data = {'channel_number': circuit.channel_number,
+                        'direction': circuit.energy_direction}
+        if circuit.channel_number < 4:
+            circuit_data['type'] = 'Mains'
+        else:
+            circuit_data['type'] = circuit.sub_type
+        for channel in usage_response.device_usages[0].channel_usages:
+            if channel.channel == circuit.channel_number:
+                circuit_data['usage'] = channel.usages[0]
+        # Only return circuits with usage
+        if 'usage' in circuit_data:
+            response.append(circuit_data)
+
+    return response
+
+
+# (Currently known) Circuit types
+def get_usage_by_circuit_type(usage_data: List[dict], circuit_type: str):
+    """
+    Currently known circuit types:
+    'Clothes Dryer', 'Cooktop/Range/Oven/Stove', 'Clothes Washer', 'Battery',
+    'Water Heater', 'Humidifier/Dehumidifier', 'Fridge/Freezer','Air Conditioner',
+    'Boiler', 'Lights', 'Sub Panel', 'Heat Pump', 'Dishwasher', 'Other', 'Microwave',
+    'Pump', 'Garage/Shop/Barn/Shed', 'Kitchen', 'Solar/Generation', 'Room/Multi-use Circuit',
+    'Electric Vehicle/RV', 'Hot Tub/Spa', 'Computer/Network', 'Furnace'
+    """
+    total_usage = 0
+    found_circuits = 0
+
+    for device in usage_data:
+        for circuit in device:
+            if circuit['type'] == circuit_type:
+                total_usage += circuit['usage']
+                found_circuits += 1
+    return total_usage, found_circuits
+
+
+def get_detailed_energy_details():
+
+    now = math.ceil(time.time())
+
+    print(usage_request(devices[0], now))
+
+    #all_usages = [usage_request(device, now) for device in devices[:5]]
+    pass
 
 
 def get_energy_summary():
@@ -83,7 +124,7 @@ def get_energy_summary():
     usage_request.start_epoch_seconds = now - 900  # one fifteen minute period
     usage_request.end_epoch_seconds = now
     usage_request.scale = DataResolution.FifteenMinutes
-    usage_request.channels = DeviceUsageRequest.UsageChannel.MAINS # Change to ALL when getting individual circuits
+    usage_request.channels = DeviceUsageRequest.UsageChannel.MAINS
     usage_request.manufacturer_device_ids.extend([_.manufacturer_device_id for _ in devices])
 
     usage_response = stub.GetUsageData(usage_request)
@@ -102,19 +143,9 @@ def get_energy_summary():
 
     print(timestamp, sum(usages), len(usages))
 
-    #
-    #
-    # print(f'Here are the circuit_infos describing the circuits available on this device:');
-    # for circuitInfo in vue2.circuit_infos:
-    #     # this print converts to the correct enum value and then concatenates all 5 of these strings onto a single line
-    #     print(f'{circuitInfo.channel_number:2}'
-    #           f'{DeviceInventoryResponse.Device.CircuitInfo.CircuitType.Name(circuitInfo.type):20}'
-    #           f'{DeviceInventoryResponse.Device.CircuitInfo.EnergyDirection.Name(circuitInfo.energy_direction):20}'
-    #           f'{circuitInfo.sub_type:20}'
-    #           f'{circuitInfo.name:20}')
-
 
 if __name__ == "__main__":
 
+    #get_detailed_energy_details()
     get_energy_summary()
     sys.exit(0)

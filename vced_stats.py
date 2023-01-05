1f8ccc11d5cf52b1e796e554b66094a25dbbf8f8
Alan Buss <al@constellationwebservices.com>
2023-01-05 08:49:44 -0600

    get last timestamp indent block, return time if no response

--- vced_stats.py
+++ vced_stats.py
@@ -46,6 +46,7 @@ def write_to_db(values: List[dict]) -> None:
     with closing(mysql.connector.connect(**config['db'])) as conn:
         with closing(conn.cursor()) as cur:
             for data in values:
+                print(data)
                 cur.execute(
                     'INSERT IGNORE INTO usage_data (device_id, channel_id, channel_type, channel_direction, channel_usage, timestamp) VALUES (%s, %s, %s, %s, %s, %s);',
                     [data['device_id'], data['channel_id'], data['channel_type'], data['channel_direction'], data['channel_usage'],
@@ -57,22 +58,22 @@ def get_most_recent_timestamp() -> (int, int):
     with closing(mysql.connector.connect(**config['db'])) as conn:
         with closing(conn.cursor()) as cur:
             cur.execute('SELECT max(timestamp) AS most_recent FROM usage_data;', [])
-        # TODO: I'm not positive what the mysql cursor returns - the next line may
-        #  need to be replaced with
-        #  return cur.fetchone()['most_recent']
-        try:
-            # Overlap by 31 minutes to make sure no data is missed
-            since = cur.fetchone()[0] - 1860
-            one_week_ago = int(time.time()) - 604800
-            if since < one_week_ago:
-                print("Warning! No data records detected for more than a week. Fetching the next needed week "
-                      "rather than the most recent week.")
-                return since, since + 604800
-            else:
-                return since, None
-        except:
-            print('Detected first run, getting data for last week. If seen more than once, this code is buggy.')
-            return int(time.time()) - 604800, None
+            # TODO: I'm not positive what the mysql cursor returns - the next line may
+            #  need to be replaced with
+            #  return cur.fetchone()['most_recent']
+            try:
+                # Overlap by 31 minutes to make sure no data is missed
+                since = cur.fetchone()[0] - 1860
+                one_week_ago = int(time.time()) - 604800
+                if since < one_week_ago:
+                    print("Warning! No data records detected for more than a week. Fetching the next needed week "
+                          "rather than the most recent week.")
+                    return since, since + 604800
+                else:
+                    return since, None
+            except:
+                print('Detected first run, getting data for last week. If seen more than once, this code is buggy.')
+                return int(time.time()) - 604800, int(time.time())
 
 
 def store_detailed_usage(since: int, until: int = None) -> List[dict]:
